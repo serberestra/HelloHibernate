81fb4c99de755bd9742583ef7bcf08467b0a00aa
serberestra <serberestra@gmail.com>
2020-05-31 12:43:10 -0400

    Corrections v1

--- src/main/java/com/example/servlet/MasterServlet.java
+++ src/main/java/com/example/servlet/MasterServlet.java
@@ -3,14 +3,12 @@ package com.example.servlet;
 import java.io.IOException;
 import java.rmi.ServerException;
 
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.example.controller.RequestHelper;
 
-@WebServlet("/api/*")
 public class MasterServlet extends HttpServlet{
 
 	private static final long serialVersionUID = 8843192743095604877L;
